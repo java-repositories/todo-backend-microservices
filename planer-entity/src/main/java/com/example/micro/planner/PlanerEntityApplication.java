d256806ef60a582dc706f345d00b51cbc85e2fff
atdushi <atdushi@users.noreply.github.com>
2024-10-09 17:55:31 +0300

    config + eureka + gateway + entities

---
+++ planer-entity/src/main/java/com/example/micro/planner/PlanerEntityApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanerEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanerEntityApplication.class, args);
+    }
+
+}
---
+++ planner-gateway/src/main/java/com/example/micro/planner/plannergateway/PlannerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro.planner.plannergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerGatewayApplication.class, args);
+	}
+
+}
