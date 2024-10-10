02003862ff200b159f804ab41850905095f6cc9e
atdushi <atdushi@users.noreply.github.com>
2024-10-10 10:55:33 +0300

    rename planner-entity

--- planner-entity/src/main/java/com/example/micro/planner/PlannerEntityApplication.java
+++ planner-entity/src/main/java/com/example/micro/planner/PlannerEntityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlanerEntityApplication {
+public class PlannerEntityApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PlanerEntityApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerEntityApplication.class, args);
+	}
 
 }
